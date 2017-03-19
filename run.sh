d5b95189f72f8c4beb58704709e210017850b0c5
Ben <benyohay91@gmail.com>
2017-03-20 00:35:17 +0200

    fix run.sh to pass args to remarkable.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 foldername=`dirname $(readlink -f $0)`
-PYTHONPATH=$foldername/remarkable:$foldername/remarkable_lib exec $foldername/bin/remarkable
+PYTHONPATH=$foldername/remarkable:$foldername/remarkable_lib exec $foldername/bin/remarkable "$@"
