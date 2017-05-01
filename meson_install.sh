2448a8f9130f6120bbb620d0771e404f771733fc
Christian Fredrik Kalager Schaller <uraeus@linuxrising.org>
2017-05-01 13:30:14 -0400

    Add Meson build system

---
+++ meson_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Update icon caches
+gtk-update-icon-cache -f -t ${DESTDIR}/${MESON_INSTALL_PREFIX}/share/icons/hicolor
+gtk-update-icon-cache -f -t ${DESTDIR}/${MESON_INSTALL_PREFIX}/share/icons/HighContrast
+
+# Install new schemas
+glib-compile-schemas ${DESTDIR}/${MESON_INSTALL_PREFIX}/share/glib-2.0/schemas/
