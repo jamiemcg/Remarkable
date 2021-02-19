bea2b5e4ccd9158e25f14461df599fb51e9c7643
Joseph C Wang <joequant@gmail.com>
2021-02-19 12:27:36 +0800

    fix shabang typo

--- remarkable/RemarkableWindow.py
+++ remarkable/RemarkableWindow.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 # Copyright (C) 2020 <Jamie McGowan> <jamiemcgowan.dev@gmail.com>
