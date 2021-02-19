4430f1bf3d4ceee1e65a52665d10de29655d1df4
Joseph C Wang <joequant@gmail.com>
2021-02-19 10:43:38 +0800

    change imports to be path independent

--- remarkable/RemarkableWindow.py
+++ remarkable/RemarkableWindow.py
@@ -36,10 +36,10 @@ import pdfkit
 import re, subprocess, datetime, os, webbrowser, _thread, sys, locale
 import tempfile
 import traceback
-import styles
+from remarkable import styles
 import unicodedata
 import warnings
-from findBar import FindBar
+from .findBar import FindBar
 
 # Check if gtkspellcheck is installed
 try:
